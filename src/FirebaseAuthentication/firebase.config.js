bd27e0ee4e6fd43f4a76263af20b5c784d12b207
zihad868 <zihadh868@gmail.com>
2024-08-14 23:18:09 +0600

    Firebase Hosting

---
+++ src/FirebaseAuthentication/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5joDC7foi3CvM_R58grbsqpr1y5V5yXk",
+  authDomain: "shopsphere-cc06b.firebaseapp.com",
+  projectId: "shopsphere-cc06b",
+  storageBucket: "shopsphere-cc06b.appspot.com",
+  messagingSenderId: "455554473421",
+  appId: "1:455554473421:web:c36a2b8772b57484a3b884"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
