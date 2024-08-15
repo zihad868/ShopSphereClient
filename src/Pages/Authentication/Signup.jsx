c4919ab0484ff9d50ae54e2f3b423beb51e064cb
zihad868 <zihadh868@gmail.com>
2024-08-15 23:22:00 +0600

    Authentication

--- src/Pages/Authentication/Signup.jsx
+++ src/Pages/Authentication/Signup.jsx
@@ -1,46 +1,74 @@
+import { FcGoogle } from "react-icons/fc";
 import { useForm } from "react-hook-form";
 import signUp from "../../assets/Authentication/signUp.jpg";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 
 const Signup = () => {
+  const [customError, setError] = useState("");
   const { signUpUser } = useContext(AuthContext);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = async(data) => {
-    const formData = new FormData()
-    formData.append('image', data.image[0])
 
-    try{
-      const response = await fetch( `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMAGE_BB_API}`, {
-        method: 'POST',
-        body: formData
-      })
+  const handleSignupGoogle = () => {
+    console.log("google Signup")
+  }
+
+
+  const onSubmit = async (data) => {
+    const { name, email, password } = data;
+
+    if (password.length < 6) {
+      setError("Password at least 6 character");
+      return;
+    }
+
+    setError("");
+
+    const formData = new FormData();
+    formData.append("image", data.image[0]);
+
+    try {
+      const response = await fetch(
+        `https://api.imgbb.com/1/upload?key=${
+          import.meta.env.VITE_IMAGE_BB_API
+        }`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
 
       const result = await response.json();
       const imgURL = result.data.display_url;
 
-      console.log(imgURL)
-
-    }catch(error){
-      console.error(error)
+      console.log(imgURL, name, email, password);
+    } catch (error) {
+      console.error(error);
     }
-  }
+  };
 
   return (
     <>
       <h1 className="text-center text-2xl font-bold text-sky-400">
         Sign-up Now
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 m-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 m-5">
         <div>
           <img src={signUp} alt="" />
         </div>
-        <div className="mt-20">
+
+        <div className="mt-15">
+          <button onClick={handleSignupGoogle} className="border-2 p-2 bg-sky-200 hover:bg-sky-300 w-full rounded-md">
+            <div className="flex  items-center justify-center ">
+              <FcGoogle className="w-16" /> Signup With Google
+            </div>
+          </button>
+
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control">
               <label className="label">
@@ -86,14 +114,19 @@ const Signup = () => {
               {errors.password && (
                 <span className="text-red-400">Password field is required</span>
               )}
+              {customError && (
+                <span className="text-red-400">{customError}</span>
+              )}
             </div>
 
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Upload Image</span>
               </label>
-              <input type="file" class="file:border file:border-solid ..." 
-              {...register("image", {required: true})}
+              <input
+                type="file"
+                class="file:border file:border-solid ..."
+                {...register("image", { required: true })}
               />
               {errors.image && (
                 <span className="text-red-400">Image field is required</span>
